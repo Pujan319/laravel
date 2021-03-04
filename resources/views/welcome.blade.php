123d6181e21b4167624dae7c3d8984b0ccde37cc
pujan319 <pujanthapa319@gmail.com>
2021-03-04 08:31:56 +0545

    second comit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<style type="text/css">
+    h1{
+        text-transform: uppercase;
+    }
+</style>
+<body>
+    <h1>hello world</h1>
+</body>
+</html>
